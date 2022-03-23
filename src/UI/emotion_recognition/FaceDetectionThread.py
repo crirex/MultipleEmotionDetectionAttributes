344c1c91cb77873c24025c7cc97945312b10d1cc
Bogdan <bogdybv25@gmail.com>
2022-03-24 00:39:27 +0200

    Added observer pattern. To be used in video and audio threads.

--- src/UI/emotion_recognition/FaceDetectionThread.py
+++ src/UI/emotion_recognition/FaceDetectionThread.py
@@ -1,3 +1,4 @@
+from utils.Logger import Logger
 from utils.Manager import Manager
 import numpy as np
 from scipy.ndimage import zoom
@@ -8,6 +9,8 @@ import cv2
 from PySide6.QtCore import QThread
 from PIL import Image
 from PySide6.QtWidgets import QMessageBox
+
+from utils.observer import Subject
 
 
 def eye_aspect_ratio(eye):
@@ -22,30 +25,44 @@ def is_face_detected(face):
     return face.shape[0] != 0 and face.shape[1] != 0
 
 
-class FaceDetectionThread(QThread):
+class FaceDetectionThread(QThread, Subject):
     def __init__(self, parent=None):
         QThread.__init__(self, parent)
-        self.calling_window = parent
-        self.is_running = True
+        self._calling_window = parent
+        self._is_running = True
+        self._logger = Logger()
 
-        self.shape_x = 48
-        self.shape_y = 48
+        self._shape_x = 48
+        self._shape_y = 48
 
-        self.thresh = 0.17
-        self.nClasses = 7
+        self._ear_thresh = 0.17
+        self._no_classes = 7
 
-        self.nose_bridge = [28, 29, 30, 31, 33, 34, 35]
-        self.manager = Manager()
-        self.face_detect = dlib.get_frontal_face_detector()
+        self._nose_bridge = [28, 29, 30, 31, 33, 34, 35]
+        self._manager = Manager()
+        self._face_detect = dlib.get_frontal_face_detector()
 
-        self.frames = []
+        self._frames = []
+
+        self._observers = []
+
+    def attach(self, observer):
+        self._observers.append(observer)
+
+    def detach(self, observer):
+        self._observers.remove(observer)
+
+    def notify(self):
+        # here should go some logic
+        # iterate over every observer and do stuff
+        pass
 
     def stop_running(self):
-        self.is_running = False
+        self._is_running = False
 
     def get_frame(self):
-        if len(self.frames) > 0:
-            return self.frames.pop(0)
+        if len(self._frames) > 0:
+            return self._frames.pop(0)
         return None
 
     def detect_face(self, frame):
@@ -58,7 +75,7 @@ class FaceDetectionThread(QThread):
 
         # Cascade MultiScale classifier
         detected_faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6,
-                                                      minSize=(self.shape_x, self.shape_y),
+                                                      minSize=(self._shape_x, self._shape_y),
                                                       flags=cv2.CASCADE_SCALE_IMAGE)
         coord = []
 
@@ -76,7 +93,7 @@ class FaceDetectionThread(QThread):
         nose_bridge_x = []
         nose_bridge_y = []
 
-        for i in self.nose_bridge:
+        for i in self._nose_bridge:
             nose_bridge_x.append(landmarks[i][0])
             nose_bridge_y.append(landmarks[i][1])
 
@@ -103,7 +120,7 @@ class FaceDetectionThread(QThread):
         return False
 
     def run(self):
-        self.is_running = True
+        self._is_running = True
 
         (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
         (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
@@ -115,141 +132,145 @@ class FaceDetectionThread(QThread):
         (eblStart, eblEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
         (ebrStart, ebrEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
 
-        if self.manager.activeCamera is None or not self.manager.activeCamera.isOpened():
+        if self._manager.activeCamera is None or not self._manager.activeCamera.isOpened():
             QMessageBox.warning(None, "Video", "There is no video input device available.")
-            self.calling_window.ui.labelVideo.setText("No camera detected")
+            self._calling_window.ui.labelVideo.setText("No camera detected")
             return
-        
-        while self.is_running:
-            ret, frame = self.manager.activeCamera.read()
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            rects = self.face_detect(gray, 0)
+        try:
+            while self._is_running:
+                ret, frame = self._manager.activeCamera.read()
 
-            for (i, rect) in enumerate(rects):
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                rects = self._face_detect(gray, 0)
 
-                shape_img = self.manager.videoPredictorLandmarks(gray, rect)
-                shape = face_utils.shape_to_np(shape_img)
+                for (i, rect) in enumerate(rects):
 
-                (x, y, w, h) = face_utils.rect_to_bb(rect)
-                face = gray[y:y + h, x:x + w]
+                    shape_img = self._manager.videoPredictorLandmarks(gray, rect)
+                    shape = face_utils.shape_to_np(shape_img)
 
-                if not is_face_detected(face):
-                    self.frames.append(frame)
-                    continue
+                    (x, y, w, h) = face_utils.rect_to_bb(rect)
+                    face = gray[y:y + h, x:x + w]
 
-                # Zoom on extracted face
-                face = zoom(face, (self.shape_x / face.shape[0], self.shape_y / face.shape[1]))
+                    if not is_face_detected(face):
+                        self._frames.append(frame)
+                        continue
 
-                # Cast type float
-                face = face.astype(np.float32)
+                    # Zoom on extracted face
+                    face = zoom(face, (self._shape_x / face.shape[0], self._shape_y / face.shape[1]))
 
-                # Scale
-                face /= float(face.max())
-                face = np.reshape(face.flatten(), (1, 48, 48, 1))
+                    # Cast type float
+                    face = face.astype(np.float32)
 
-                # Make Prediction
-                prediction = self.manager.videoModel.predict(face)
-                prediction_result = np.argmax(prediction)
+                    # Scale
+                    face /= float(face.max())
+                    face = np.reshape(face.flatten(), (1, 48, 48, 1))
 
-                # Rectangle around the face
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    # Make Prediction
+                    prediction = self._manager.videoModel.predict(face)
+                    prediction_result = np.argmax(prediction)
 
-                cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                            (0, 255, 0), 2)
+                    # Rectangle around the face
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                for (j, k) in shape:
-                    cv2.circle(frame, (j, k), 1, (0, 0, 255), -1)
+                    cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                                (0, 255, 0), 2)
 
-                # 1. Add prediction probabilities
-                cv2.putText(frame, "----------------", (40, 100 + 180 * i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
-                cv2.putText(frame, "Emotional report : Face #" + str(i + 1), (40, 120 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
-                cv2.putText(frame, "Angry : " + str(round(prediction[0][0], 3)), (40, 140 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
-                cv2.putText(frame, "Disgust : " + str(round(prediction[0][1], 3)), (40, 160 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
-                cv2.putText(frame, "Fear : " + str(round(prediction[0][2], 3)), (40, 180 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
-                cv2.putText(frame, "Happy : " + str(round(prediction[0][3], 3)), (40, 200 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
-                cv2.putText(frame, "Sad : " + str(round(prediction[0][4], 3)), (40, 220 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
-                cv2.putText(frame, "Surprise : " + str(round(prediction[0][5], 3)), (40, 240 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
-                cv2.putText(frame, "Neutral : " + str(round(prediction[0][6], 3)), (40, 260 + 180 * i),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+                    for (j, k) in shape:
+                        cv2.circle(frame, (j, k), 1, (0, 0, 255), -1)
 
-                # 2. Annotate main image with a label
-                if prediction_result == 0:
-                    cv2.putText(frame, "Angry", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif prediction_result == 1:
-                    cv2.putText(frame, "Disgust", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif prediction_result == 2:
-                    cv2.putText(frame, "Fear", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif prediction_result == 3:
-                    cv2.putText(frame, "Happy", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif prediction_result == 4:
-                    cv2.putText(frame, "Sad", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif prediction_result == 5:
-                    cv2.putText(frame, "Surprise", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                else:
-                    cv2.putText(frame, "Neutral", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    # 1. Add prediction probabilities
+                    cv2.putText(frame, "----------------", (40, 100 + 180 * i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+                    cv2.putText(frame, "Emotional report : Face #" + str(i + 1), (40, 120 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+                    cv2.putText(frame, "Angry : " + str(round(prediction[0][0], 3)), (40, 140 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+                    cv2.putText(frame, "Disgust : " + str(round(prediction[0][1], 3)), (40, 160 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+                    cv2.putText(frame, "Fear : " + str(round(prediction[0][2], 3)), (40, 180 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+                    cv2.putText(frame, "Happy : " + str(round(prediction[0][3], 3)), (40, 200 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+                    cv2.putText(frame, "Sad : " + str(round(prediction[0][4], 3)), (40, 220 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+                    cv2.putText(frame, "Surprise : " + str(round(prediction[0][5], 3)), (40, 240 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+                    cv2.putText(frame, "Neutral : " + str(round(prediction[0][6], 3)), (40, 260 + 180 * i),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
 
-                # 3. Eye Detection and Blink Count
-                leftEye = shape[lStart:lEnd]
-                rightEye = shape[rStart:rEnd]
+                    # 2. Annotate main image with a label
+                    if prediction_result == 0:
+                        cv2.putText(frame, "Angry", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    elif prediction_result == 1:
+                        cv2.putText(frame, "Disgust", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    elif prediction_result == 2:
+                        cv2.putText(frame, "Fear", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    elif prediction_result == 3:
+                        cv2.putText(frame, "Happy", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    elif prediction_result == 4:
+                        cv2.putText(frame, "Sad", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    elif prediction_result == 5:
+                        cv2.putText(frame, "Surprise", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    else:
+                        cv2.putText(frame, "Neutral", (x + w - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-                # Compute Eye Aspect Ratio
-                leftEAR = eye_aspect_ratio(leftEye)
-                rightEAR = eye_aspect_ratio(rightEye)
-                ear = (leftEAR + rightEAR) / 2.0
+                    # 3. Eye Detection and Blink Count
+                    leftEye = shape[lStart:lEnd]
+                    rightEye = shape[rStart:rEnd]
 
-                # Output Eye Detection Results
-                cv2.putText(frame, "Eyes " + ("Closed" if ear < self.thresh else "Opened"), (40, 400),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
-                print("EAR: " + str(ear))
+                    # Compute Eye Aspect Ratio
+                    leftEAR = eye_aspect_ratio(leftEye)
+                    rightEAR = eye_aspect_ratio(rightEye)
+                    ear = (leftEAR + rightEAR) / 2.0
 
-                # Output Glasses Detection
-                has_glasses = str(self.has_glasses(shape_img, gray))
-                cv2.putText(frame, "Glasses: " + has_glasses, (40, 380),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
-                print("Glasses: " + has_glasses)
+                    # Output Eye Detection Results
+                    cv2.putText(frame, "Eyes " + ("Closed" if ear < self._ear_thresh else "Opened"), (40, 400),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
+                    print("EAR: " + str(ear))
 
-                # And plot its contours
-                leftEyeHull = cv2.convexHull(leftEye)
-                rightEyeHull = cv2.convexHull(rightEye)
-                cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-                cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+                    # Output Glasses Detection
+                    has_glasses = str(self.has_glasses(shape_img, gray))
+                    cv2.putText(frame, "Glasses: " + has_glasses, (40, 380),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
+                    print("Glasses: " + has_glasses)
 
-                # 4. Detect Nose
-                nose = shape[nStart:nEnd]
-                noseHull = cv2.convexHull(nose)
-                cv2.drawContours(frame, [noseHull], -1, (0, 255, 0), 1)
+                    # And plot its contours
+                    leftEyeHull = cv2.convexHull(leftEye)
+                    rightEyeHull = cv2.convexHull(rightEye)
+                    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+                    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-                # 5. Detect Mouth
-                mouth = shape[mStart:mEnd]
-                mouthHull = cv2.convexHull(mouth)
-                cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+                    # 4. Detect Nose
+                    nose = shape[nStart:nEnd]
+                    noseHull = cv2.convexHull(nose)
+                    cv2.drawContours(frame, [noseHull], -1, (0, 255, 0), 1)
 
-                # 6. Detect Jaw
-                jaw = shape[jStart:jEnd]
-                jawHull = cv2.convexHull(jaw)
-                cv2.drawContours(frame, [jawHull], -1, (0, 255, 0), 1)
+                    # 5. Detect Mouth
+                    mouth = shape[mStart:mEnd]
+                    mouthHull = cv2.convexHull(mouth)
+                    cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
 
-                # 7. Detect Eyebrows
-                ebr = shape[ebrStart:ebrEnd]
-                ebrHull = cv2.convexHull(ebr)
-                cv2.drawContours(frame, [ebrHull], -1, (0, 255, 0), 1)
-                ebl = shape[eblStart:eblEnd]
-                eblHull = cv2.convexHull(ebl)
-                cv2.drawContours(frame, [eblHull], -1, (0, 255, 0), 1)
+                    # 6. Detect Jaw
+                    jaw = shape[jStart:jEnd]
+                    jawHull = cv2.convexHull(jaw)
+                    cv2.drawContours(frame, [jawHull], -1, (0, 255, 0), 1)
 
-            cv2.putText(frame, 'Number of Faces : ' + str(len(rects)), (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, 155, 1)
+                    # 7. Detect Eyebrows
+                    ebr = shape[ebrStart:ebrEnd]
+                    ebrHull = cv2.convexHull(ebr)
+                    cv2.drawContours(frame, [ebrHull], -1, (0, 255, 0), 1)
+                    ebl = shape[eblStart:eblEnd]
+                    eblHull = cv2.convexHull(ebl)
+                    cv2.drawContours(frame, [eblHull], -1, (0, 255, 0), 1)
 
-            self.frames.append(frame)
+                cv2.putText(frame, 'Number of Faces : ' + str(len(rects)), (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, 155, 1)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                self._frames.append(frame)
 
-        # self.manager.activeCamera.release()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        except Exception as ex:
+            self._logger.log_error(ex)
+            self._is_running = False
+            self._manager.activeCamera.release()
+            raise Exception(ex)
