5ad1e713bf42057213f19c3cfe05d7151226097f
Bogdan <bogdybv25@gmail.com>
2022-03-29 22:20:49 +0300

    Added second input for voice as temporary solution for emotion detection

--- src/UI/widgets/audio_plotting/AudioPlotter.py
+++ src/UI/widgets/audio_plotting/AudioPlotter.py
@@ -7,6 +7,14 @@ SAMPLE_COUNT = 2000
 RESOLUTION = 4
 
 
+def get_audio_format(no_channels=1, sample_rate=16000, sample_format=QAudioFormat.Int16):
+    format_audio = QAudioFormat()
+    format_audio.setChannelCount(no_channels)
+    format_audio.setSampleRate(sample_rate)
+    format_audio.setSampleFormat(sample_format)
+    return format_audio
+
+
 class AudioPlotter(QChartView):
     def __init__(self, parent):
         super().__init__(parent=parent)
@@ -16,48 +24,70 @@ class AudioPlotter(QChartView):
             QMessageBox.warning(None, "audio", "There is no audio input device available.")
 
         self.device = input_devices[0]
+        self._io_device_plotting = None
         self._io_device = None
         self._buffer = [QPointF(x, 0) for x in range(SAMPLE_COUNT)]
         self._series = QLineSeries()
         self._chart = QChart()
         self._chart.addSeries(self._series)
+
         self._axis_x = QValueAxis()
         self._axis_x.setRange(0, SAMPLE_COUNT)
         self._axis_x.setLabelFormat("%g")
         self._axis_x.setTitleText("Samples")
+
         self._axis_y = QValueAxis()
         self._axis_y.setRange(-1, 1)
         self._axis_y.setTitleText("Audio level")
+
         self._chart.setAxisX(self._axis_x, self._series)
         self._chart.setAxisY(self._axis_y, self._series)
         self._chart.legend().hide()
+
         name = self.device.description()
         self._chart.setTitle(f"Data from the microphone ({name})")
         self.setChart(self._chart)
         self._series.append(self._buffer)
 
-        format_audio = QAudioFormat()
-        format_audio.setSampleRate(8000)
-        format_audio.setChannelCount(1)
-        format_audio.setSampleFormat(QAudioFormat.UInt8)
+        self._audio_input_plotting = QAudioSource(self.device, get_audio_format(1, 8000, QAudioFormat.UInt8), self)
+        self._audio_input = QAudioSource(self.device, get_audio_format(), self)
 
-        self._audio_input = QAudioSource(self.device, format_audio, self)
+        self._frames = []
 
     def start_recording(self):
+        self._io_device_plotting = self._audio_input_plotting.start()
+        self._io_device_plotting.readyRead.connect(self._readyRead)
+
         self._io_device = self._audio_input.start()
-        self._io_device.readyRead.connect(self._readyRead)
+        self._io_device.readyRead.connect(self._store_data)
 
     def stop_recording(self):
-        if self._audio_input is not None:
+        if self._audio_input_plotting is not None \
+                and self._audio_input is not None:
+            self._audio_input_plotting.stop()
             self._audio_input.stop()
+
+            if __debug__:
+                import wave
+                wf = wave.open("test.wav", 'w')
+                wf.setnchannels(1)
+                wf.setsampwidth(2)
+                wf.setframerate(16000)
+                wf.writeframes(b''.join(self._frames))
+                wf.close()
 
     def closeEvent(self, event):
         self.stop_recording()
         event.accept()
 
     @Slot()
+    def _store_data(self):
+        self._frames.append(self._io_device.readAll().data())
+
+    @Slot()
     def _readyRead(self):
-        data = self._io_device.readAll()
+        data = self._io_device_plotting.readAll()
+
         available_samples = data.size() // RESOLUTION
         start = 0
         if available_samples < SAMPLE_COUNT:
