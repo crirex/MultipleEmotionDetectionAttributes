b0f736c9a218497cb04b638db890f518adc84bc0
Crirex <crirex5k@gmail.com>
2022-06-21 04:00:43 +0300

    Added some plots to the pdf file

--- src/UI/widgets/report_table/ReportExporter.py
+++ src/UI/widgets/report_table/ReportExporter.py
@@ -4,18 +4,48 @@ from collections import Counter
 from persistance import MongoDb
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
 
 time_format = "%Y-%m-%d %H-%M"
 
 
-def plot_bar(predictions, fig):
+def plot_bar(predictions, title, subplot, fig):
+    ax = fig.add_subplot(subplot)
+    plt.xlabel('Emotions')
+    plt.ylabel('Count')
     if predictions is None or len(predictions) == 0:
+        plt.title("No " + title)
         return
 
     emotions = [value[1] for value in predictions.values()]
     emotions_count = Counter(emotions)
-    ax = fig.add_subplot(111)
+    plt.title(title)
     plt.bar(emotions_count.keys(), emotions_count.values(), color='red', lw=5)
+
+
+def plot(predictions, title, subplot, fig):
+    ax = fig.add_subplot(subplot)
+    plt.xlabel('Seconds')
+    plt.ylabel('Emotions')
+    if predictions is None or len(predictions) == 0:
+        plt.title("No " + title)
+        return
+
+    emotions = [value[1] for value in predictions.values()]
+    plt.title(title)
+
+    plt.plot(np.arange(len(emotions)) * 4, emotions, color='blue')
+
+
+def plot_bar_text(predictions, title, subplot, fig):
+    ax = fig.add_subplot(subplot)
+    plt.xlabel('Emotions')
+    if predictions is None or len(predictions) == 0:
+        plt.title("No " + title)
+        return
+
+    plt.title(title)
+    plt.bar(predictions.keys(), predictions.values(), color='green', lw=5)
 
 
 def save_multi_image(filename):
@@ -39,19 +69,21 @@ class ReportExporter:
         plt.rcParams["figure.figsize"] = [7.00, 3.50]
         plt.rcParams["figure.autolayout"] = True
 
-        fig = plt.figure()
-        plot_bar(predictions.audio_predictions, fig)
+        fig_audio = plt.figure()
+        plot_bar(predictions.audio_predictions, "Total Audio Predictions", 211, fig_audio)
+        plot(predictions.audio_predictions, "Audio Predictions Each Second", 212, fig_audio)
 
-        # plt.plot(random_shit, [2, 1, 7, 1, 2], color='red', lw=5)
-        # ax2 = fig.add_subplot(312)
-        # plt.bar(random_shit, [3, 5, 1, 5, 3], color='green', lw=5)
-        # plt.bar(random_shit, [11, 3, 4, 2, 9], color='blue', lw=5)
-        # plt.plot(random_shit, [2, 1, 7, 1, 2], color='red', lw=5)
+        fig_video = plt.figure()
+        plot_bar(predictions.video_predictions, "Total Video Predictions", 211, fig_video)
+        plot(predictions.video_predictions, "Video Predictions Each Second", 212, fig_video)
 
-        # ax3 = fig.add_subplot(313)
-        # plt.plot([1, 2, 3, 4, 5], color='yellow', lw=5)
+        fig_text = plt.figure()
+        plot_bar_text(predictions.text_predictions, "Total Text Predictions", 111, fig_text)
 
         filename = self._path + f"\\{report['interviewee_name']}-" \
                                 f"{report['interviewer_name']}-" \
                                 f"{time.strftime(time_format, time.localtime(report['interview_start_date']))}.pdf"
-        save_multi_image(filename)
+        try:
+            save_multi_image(filename)
+        finally:
+            plt.close("all")
